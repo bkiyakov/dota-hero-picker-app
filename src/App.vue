b2a0ba9063898cddb6242a5f04bb524372c57a6e
bkiyakov <bkiyakov@yandex.ru>
2019-11-16 11:34:28 +0300

    added just 3 heroes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main">
+    <HeroCard name="Mirana" img="https://gamepedia.cursecdn.com/dota2_gamepedia/1/12/Mirana_icon.png"/>
+    <HeroCard name="Ember Spirit" img="https://liquipedia.net/commons/images/9/98/Ember_Spirit_Large.png"/>
+    <HeroCard name="Monkey King" img="https://gamepedia.cursecdn.com/dota2_gamepedia/7/7b/Monkey_King_icon.png"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeroCard from './components/HeroCard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HeroCard
   }
 }
 </script>
@@ -24,5 +25,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#main {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/HeroCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="hero-card">
+        <h2>{{name}}</h2>
+        <img :src="img" alt="Hero image"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeroCard",
+    props: {
+        name: String,
+        img: String 
+    }
+}
+</script>
+
+<style scoped>
+    .hero-card {
+        margin: 35px;
+        text-align: center;
+        transition: all 500ms ease;
+        border: 1px solid black;
+    }
+    
+    .hero-card:hover {
+        -webkit-transform: scale(1.1);
+        -ms-transform: scale(1.1);
+        transform: scale(1.1);
+    }
+
+    img {
+        margin: 0 10px 0 10px;
+    }
+
+    @keyframes bounce {
+	0%, 20%, 60%, 100% {
+		-webkit-transform: translateY(0);
+		transform: translateY(0);
+	}
+
+	40% {
+		-webkit-transform: translateY(-20px);
+		transform: translateY(-20px);
+	}
+
+	80% {
+		-webkit-transform: translateY(-10px);
+		transform: translateY(-10px);
+	}
+}
+</style>
