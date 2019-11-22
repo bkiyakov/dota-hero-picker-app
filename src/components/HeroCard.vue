d61fc005fab1eaa4630f01b09cf55949cdc07a6b
bkiyakov <bkiyakov@yandex.ru>
2019-11-22 16:29:06 +0300

    added attribute checker

--- src/components/HeroCard.vue
+++ src/components/HeroCard.vue
@@ -6,29 +6,31 @@
             <li>
                 <p v-bind:key="hero.roles.indexOf(role)" v-for="role in hero.roles">{{role}}</p>
             </li>
-            <li>{{attrString(hero.primary_attr)}}</li>
+            <hr>
+            <li>{{attr}}</li>
         </ul>
     </div>
 </template>
 
 <script>
 
-const attrString = attr => {
-    switch (attr) {
-        case "agi":
-            return "Agility";
-        case "str":
-            return "Strength";
-        case "int":
-            return "Intilligence";
-        default:
-            return "Unrecognized";
-    }
-}
-
 export default {
     name: "HeroCard",
-    props: ["hero"]
+    props: ["hero"],
+    computed: {
+        attr: function(){
+            switch (this.hero.primary_attr) {
+                case "agi":
+                    return "Agility";
+                case "str":
+                    return "Strength";
+                case "int":
+                    return "Intelligence";
+                default:
+                    return "Default";
+            }
+        }
+    }
 }
 </script>
 
@@ -54,6 +56,10 @@ export default {
         text-align: center;
         padding: 0;
         list-style-type: none;
+    }
+
+    li {
+        margin-top: 5px;
     }
 
     p {
